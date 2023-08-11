c68153b0070b14df98958fbe7ce3bec0819dbaeb
fahizrahmank <fahizrahmank@gmail.com>
2023-08-11 15:23:03 +0530

    new update

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,8 @@ import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addtodo, removetodo } from "./UserReducer";
 import { useNavigate } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function Home() {
   const reff = useRef();
@@ -24,19 +26,35 @@ function Home() {
 
   return (
     <div>
-      <input ref={reff} type="text" placeholder="type here" />
-      <button onClick={handleClick}>Add</button>
-      <ul>
-        {todoList.map((e) => (
-          <li key={e.id}>
-            {e.added}
-            <button onClick={() => nav(`/edit/${e.id}`)}>edit</button>
-            <button id={e.id} onClick={delet}>
-              delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: "center" }}>
+        <h1>TODOD APP</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
+        {/* <input /> */}
+        <InputGroup size="lg">
+        <InputGroup.Text id="inputGroup-sizing-lg" ref={reff} type="text" placeholder="type here" ></InputGroup.Text>
+        <Form.Control
+          aria-label="Large"
+          aria-describedby="inputGroup-sizing-sm"
+        />
+      </InputGroup>
+        <button onClick={handleClick}>Add</button>
+        <ul>
+          {todoList.map((e) => (
+            <li key={e.id}>
+              {e.added}
+              <button onClick={() => nav(`/edit/${e.id}`)}>edit</button>
+              <button id={e.id} onClick={delet}>
+                delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
